eca8541299c0472b1430ead84e0d0faaf4337ddf
Marveleecious <richardtimilehin05@gmail.com>
2022-10-13 16:49:53 -0700

    my first statement

---
+++ script.js
@@ -0,0 +1,24 @@
+output = document.getElementById('output');
+let scores = [60, 77, 38, 19, 59, 47, 23];
+
+
+
+for(let m = 0; m < scores.length; m++) {
+    let punishment = scores[m] - 5
+    let manipulated = (punishment + scores[m]) / 2
+
+    console.log(punishment)
+
+    if (punishment > 50){
+        output.innerHTML += punishment + "- you passed wonderfully. your previous score is " + scores[m] + " after manipulation <br> "
+
+    }
+
+    else if ((punishment + scores[m]) / 2 > 30){
+        output.innerHTML += manipulated + "- you escaped my punishment because of my manipulated score. your previous score is " + scores[m] + "  after manipulation <br>"
+    }
+
+    else {
+        output.innerHTML += punishment + "- you failed badly. your previous score is " + scores[m] + "  after manipulation <br>"
+    }
+}
